ee14e065b4ee0d5de06a995053d8b4774f490589
AlexKira <60853187+AlexKira@users.noreply.github.com>
2020-08-27 18:18:25 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-
-
-# Create your models here.
-
-class Topic(models.Model):
-	text = models.CharField(max_length=200)
-	date_added = models.DateTimeField(auto_now_add=True)
-	def_str_(self):
-		return self.text
